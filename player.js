86d0cf401231f12e0e872c66bf6b660b21c17322
em1kam <emilia@familiekaminski.de>
2021-10-14 15:53:18 +0200

    Initial version of the game

---
+++ player.js
@@ -0,0 +1,140 @@
+class Player {
+    constructor(plan, end) {
+      this.state = "standing"; //rechts,links,oben,unten
+    }
+  
+    paint(x, y) {
+      if (end) {
+        fill("green");
+        rect((50 * x) / 2, (y * 50) / 2, 50, 50);
+        console.log("geschafft");
+      } else {
+        fill("blue");
+        rect((50 * x) / 2, (y * 50) / 2, 50, 50);
+      }
+      fill("white");
+      circle((x * 50) / 2 + 25, (y * 50) / 2 + 25, 25);
+    }
+  
+    move() {
+      let dx = 0;
+      let dy = 0;
+  
+      if (this.state === "rechts") {
+        dx = 2;
+      } else if (this.state === "links") {
+        dx = -2;
+      } else if (this.state === "unten") {
+        dy = 2;
+      } else if (this.state === "oben") {
+        dy = -2;
+      }
+  
+      for (let x = 0; x < plan.length; x++) {
+        for (let y = 0; y < plan[x].length; y++) {
+          if (plan[y][x] === "x") {
+            //console.log(x, y, dx, dy);
+  
+            if (
+              y + dy < 0 ||
+              y + dy > plan.length - 1 ||
+              x + dx < 0 ||
+              x + dx > plan.length - 1
+            ) {
+              plan[y][x] = "f";
+              plan[1][1] = "x";
+              console.log("wall");
+              c+=1
+            } else if (plan[y + dy/2][x + dx/2] === "w"){
+                       plan[y][x] = "f";
+              plan[1][1] = "x";
+              console.log("wall");
+              c+=1
+                       }
+            else if (plan[y + dy][x + dx] === "g") {
+              end = true;
+              plan[y][x] = "f";
+              plan[y + dy][x + dx] = "x";
+              return;
+            } else {
+              plan[y][x] = "f";
+              plan[y + dy][x + dx] = "x";
+            }
+            return;
+          }
+        }
+      }
+    }
+    checkdirec(plan, lastdirec) {
+      let resultArray = [];
+      for (let x = 0; x < plan.length; x++) {
+        for (let y = 0; y < plan[x].length; y++) {
+          if (plan[y][x] === "x") {
+            if (plan[y + 1][x] !== "w" && lastdirec !== "oben") {
+              resultArray.push("unten");
+            }
+            if (plan[y - 1][x] !== "w" && lastdirec !== "unten") {
+              resultArray.push("oben");
+            }
+            if (plan[y][x + 1] !== "w" && lastdirec !== "links") {
+              resultArray.push("rechts");
+            }
+            if (plan[y][x - 1] !== "w" && lastdirec !== "rechts") {
+              resultArray.push("links");
+            }
+          }
+        }
+      }
+      return resultArray;
+    }
+  
+    build(plan) {
+      let dx = 0;
+      let dy = 0;
+  
+      if (this.state === "rechts") {
+        dx = 2;
+      } else if (this.state === "links") {
+        dx = -2;
+      } else if (this.state === "unten") {
+        dy = 2;
+      } else if (this.state === "oben") {
+        dy = -2;
+      }
+      //console.log(dx, dy);
+      for (let x = 0; x < plan.length; x++) {
+        for (let y = 0; y < plan[x].length; y++) {
+          if (plan[y][x] === "x") {
+            // jedes Feld wird nach dem Spieler abgesucht
+            plan[y + dy][x + dx] = "n"; // n=next         // das Feld, auf das der Spieler in diesem Zug tritt, wird als solches markiert
+  
+            if (y + 2 < plan.length) {
+              if (plan[y + 2][x] === "f") {
+                plan[y + 1][x] = "w"; // wenn sich unterhalb des aktuellen Feldes ein Feld befindet, auf das nicht gezogen wird und das nicht schon bezogen worden ist: Wand setzen
+              }
+            }
+            if (y - 2 > 0) {
+              if (plan[y - 2][x] === "f") {
+                plan[y - 1][x] = "w"; // oberhalb analog zu unterhalb
+              }
+            }
+            if (x + 2 < plan.length) {
+              if (plan[y][x + 2] === "f") {
+                plan[y][x + 1] = "w"; // rechts analog zu unterhalb
+              }
+            }
+            if (x - 2 > 0) {
+              if (plan[y][x - 2] === "f") {
+                plan[y][x - 1] = "w"; // links analog zu unterhalb
+              }
+            }
+  
+            plan[y + dy][x + dx] = "x"; // der Spieler wird weiterbewegt
+            plan[y][x] = "l"; // das letzte Feld wird als "begangen" markiert
+            this.state = "standing";
+            return;
+          }
+        }
+      }
+    }
+  }
---
+++ sketch.js
@@ -0,0 +1,150 @@
+let length;
+let plan = [];
+
+let altplan = [];
+let player;
+let altplay;
+//let goaly;
+//let goalx;
+//let i = 0;
+let end = false;
+let zug = true;
+let lastdirec = "standing";
+let c = 1;
+
+
+
+function myInputEvent() {
+  length = parseInt(this.value()); // vom Benutzer wird die Länge/Breite des Spielfeldes angegeben
+
+  length = length * 2 + 1;
+  //console.log(length);
+  plan = [];
+  for (let a = 0; a < length; a++) {
+    plan.push([]);
+    for (let b = 0; b < length; b++) {
+      if (a === 0 || b === 0 || a === length - 1 || b === length - 1) {
+        plan[a].push("w");
+      } else if (a % 2 === 1 && b % 2 === 1) {
+        plan[a].push("f");
+      } else {
+        plan[a].push("");
+      }
+      if (a === 1 && b === 1) {
+        plan[a][b] = "x";
+      }
+    }
+  }
+  //console.log(plan);
+  altplan = plan;
+  player = new Player(plan);
+  altplay = new Player(altplan);
+  creategame();
+}
+
+function creategame() {
+  while (zug === true) {
+    //console.log(counter)
+    let possible = altplay.checkdirec(altplan, lastdirec);
+    //console.log(possible);
+    //console.log(altplan);
+    altplay.state = random(possible);
+    lastdirec = altplay.state;
+    if (possible.length === 0) {
+      for (let x = 0; x < altplan.length; x++) {
+        for (let y = 0; y < altplan[x].length; y++) {
+          if (altplan[y][x] === "x") {
+            altplan[y][x] = "g";
+            altplan[1][1] = "x";
+          }
+        }
+      }
+      //console.log(altplan)
+      break;
+    }
+    //console.log(altplay.state);
+    altplay.build(altplan);
+    //console.log(counter)
+    plan = altplan;
+  }
+}
+
+function setup() {
+  createCanvas(1500, 700);
+  
+ 
+  let inp = createInput("");
+
+  inp.position(0, 0);
+  inp.size(100);
+  inp.input(myInputEvent);
+}
+
+function draw() {
+  background(255, 235, 205);
+  if (end === false) {
+    textSize(35);
+    fill("gray");
+    text("Versuch: " + c, 25, (plan.length / 2) * 50 + 150);
+    textSize(25)
+    text("Regeln: \n-> gib oben die Größe deines Spielfeldes ein (auch während des Spieles möglich) \n-> je größer das Spielfeld, desto schieriger\n-> klicke dann einmal außerhalb der Textbox auf den Bildschirm \n-> du kannst den Spieler mit den Pfeiltasten bewegen \n-> wenn du gegen eine Wand läufst, wirst du zurückgesetzt \nZiel: erreiche das orangene Spielfeld",50, (plan.length / 2) * 50 + 200)
+
+    //console.log(plan)
+    for (let x = 0; x < plan.length; x++) {
+      for (let y = 0; y < plan[x].length; y++) {
+        if (plan[y][x] === "g") {
+          fill("orange");
+          rect((50 * x) / 2, (y * 50) / 2, 50, 50);
+        }
+        if (plan[y][x] === "f" || plan[y][x] === "l") {
+          fill("blue");
+          rect((50 * x) / 2, (y * 50) / 2, 50, 50); //felder sind blau
+        }
+        if (plan[y][x] === "x") {
+          player.paint(x, y); // Spieler als weißer Kreis
+        }
+      }
+    }
+  } else {
+    textSize(30);
+    fill("gray");
+    text(
+      "Super :D \n\nDu hast " +
+        c +
+        " Versuch(e) gebraucht,um das Rätsel zu lösen und das Ende zu erreichen.\n\n[drücke Enter für einen Neustart]",
+      60,
+      150
+    );
+  }
+}
+
+function keyPressed() {
+  if (end === false) {
+    if (keyCode === 37) {
+      player.state = "links";
+      player.move(c);
+      player.state = "standing";
+    } else if (keyCode === 38) {
+      player.state = "oben";
+      player.move(c);
+      player.state = "standing";
+    } else if (keyCode === 39) {
+      player.state = "rechts";
+      player.move(c);
+      player.state = "standing";
+    } else if (keyCode === 40) {
+      player.state = "unten";
+      player.move(c);
+      player.state = "standing";
+    }
+  }
+  if (keyCode === 13) {
+    length = 0;
+    plan = [];
+    altplan = [];
+    end = false;
+    zug = true;
+    lastdirec = "standing";
+    c = 1;
+  }
+}
